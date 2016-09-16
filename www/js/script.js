fe5be217d8078c67a41a483f587c77c1f45c6987
David Indra <mail@davidindra.cz>
2016-09-16 02:02:40 +0200

    Church single view

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,13 @@
-$( document ).ready(function(){
+$(document).ready(function () {
     $(".button-collapse").sideNav();
+
+    $('[data-link]')
+        .each(function () {
+            $(this).children()
+                .wrapInner('<a class="js-link" href="' + $(this).attr('data-link') + '"></a>')
+                .addClass('js-link');
+        })
+        .click(function () {
+            location.href = $(this).attr('data-link');
+        });
 });
