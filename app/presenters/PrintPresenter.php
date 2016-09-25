7b7cb93810285dd2a79079a26d4fc22ce7298a9b
David Indra <mail@davidindra.cz>
2016-09-25 16:38:41 +0200

    Export headers

--- app/presenters/PrintPresenter.php
+++ app/presenters/PrintPresenter.php
@@ -97,10 +97,6 @@ class PrintPresenter extends SecuredPresenter
 
     public function renderExport($type, $churches, $period, $breakAnnouncements, $print = false)
     {
-        /** @var Nette\Http\Response $httpResponse */
-        $httpResponse = $this->context->getByType('Nette\Http\Response');
-        $httpResponse->setHeader('X-Frame-Options', 'allowall');
-
         if(empty($type) || empty($churches) || empty($period)){
             $this->error('Formulář byl vyplněn nesprávně.', 500);
         }
