03fc23864296d85d8468b99266d7443c7aeee42e
David Indra <mail@davidindra.cz>
2016-11-17 20:34:00 +0100

    Fix

--- www/js/script.js
+++ www/js/script.js
@@ -95,10 +95,10 @@ function init(ajax = false) {
         }
     });
 
-    $('form').on('submit', function (e) {
+    /*$('form').on('submit', function (e) {
         $('[name=date]').val($('[name=date_submit]').val());
         //$(this).netteAjax(e);
-    });
+    });*/
 
     $('.timepicker').pickatime({
         twelvehour: false,
@@ -109,6 +109,10 @@ function init(ajax = false) {
 $(document).ready(init());
 
 $.nette.ext('custom', {
+    before: function () {
+        $('[name=date]').val($('[name=date_submit]').val());
+    },
+
     complete: function () {
         init(true);
     }
