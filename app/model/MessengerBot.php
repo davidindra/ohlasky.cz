e1e097a2ae4f41622994a189ab239648c3e470f3
David Indra <mail@davidindra.cz>
2016-11-18 02:40:55 +0100

    Sending

--- app/model/MessengerBot.php
+++ app/model/MessengerBot.php
@@ -2,6 +2,7 @@
 
 namespace App\Model;
 
+use GuzzleHttp\Client;
 use Nette;
 use Tracy\Debugger;
 
@@ -9,11 +10,21 @@ class MessengerBot
 {
     use Nette\SmartObject;
 
+    /**
+     * GuzzleHTTP Client instance
+     * @var Client
+     */
+    private $guzzle;
+
+    private $apiUrl = 'https://graph.facebook.com/v2.6/me/messages';
+
     private $accessToken = 'EAAP6B4JPne8BABfkuowWxrGHCkK3tbHa25ZC2JY0nJZBibiI9YSZA6Buki8ZByIlHJ8ObZCacA1gmzNI1W7BWGYe9Vy12inseS25VcVxRWabR4nZBUsFAUAXOa4Tjbzw4NKuknPwCFKLvVxt9nkgpr4vnisPwTZAEuTdO1vVq0cGwZDZD';
     private $verifyToken = '2c7dd4efbf3ad50d6d1e2b038df0c0927810fa62';
 
-    public function __construct()
+    public function __construct(Client $client)
     {
+        $this->guzzle = $client;
+
         $this->verifyProcess();
     }
 
@@ -54,9 +65,46 @@ class MessengerBot
 
     private function receivedMessage($sender, $text){
         Debugger::log($sender . ': ' . $text);
+        $this->sendMessage($sender, $text);
+    }
+
+    private function sendMessage($recipient, $text){
+        $data = [
+            'recipient' => ['id' => $recipient],
+            'message' => ['text' => $text]
+        ];
+
+        $response = $this->guzzle->post($this->apiUrl, ['form_data' => json_encode($data)]);
+            //->getBody()
+            //->getContents();
+
+        if($response->getStatusCode() != 200){
+            throw new MessengerBotException('Failed message sending.');
+        }
     }
 }
 
 class MessengerBotException extends \Exception
 {
 }
+
+
+/*request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  */
