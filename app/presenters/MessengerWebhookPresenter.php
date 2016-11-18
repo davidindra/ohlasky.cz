69e4fbf021dc2920653995a85f9d5b03b19b6d89
David Indra <mail@davidindra.cz>
2016-11-18 01:57:25 +0100

    Fix

--- app/presenters/MessengerWebhookPresenter.php
+++ app/presenters/MessengerWebhookPresenter.php
@@ -41,8 +41,8 @@ class MessengerWebhookPresenter extends BasePresenter
             //$bot->sendMessage($message->messaging->sender->id, 'text');
         }
 
-        $n = array(
-            0 => fritak\MessengerPlatform\MessageReceived::__set_state(array('id' => '560319450832679', 'time' => 1479430459825, 'messaging' => array(0 => fritak\MessengerPlatform\Messaging::__set_state(array('recipient' => fritak\MessengerPlatform\Recipient::__set_state(array('data' => array('id' => '560319450832679',),)), 'sender' => fritak\MessengerPlatform\Sender::__set_state(array('data' => array('id' => '1105714516202492',),)), 'timestamp' => 1479430034937, 'message' => fritak\MessengerPlatform\Message::__set_state(array('mid' => 23, 'seq' => NULL, 'text' => 'ahoj', 'attachments' => NULL, 'quick_reply' => NULL,)), 'delivery' => NULL, 'optin' => NULL,)),),)),);
+        /*$n = array(
+            0 => fritak\MessengerPlatform\MessageReceived::__set_state(array('id' => '560319450832679', 'time' => 1479430459825, 'messaging' => array(0 => fritak\MessengerPlatform\Messaging::__set_state(array('recipient' => fritak\MessengerPlatform\Recipient::__set_state(array('data' => array('id' => '560319450832679',),)), 'sender' => fritak\MessengerPlatform\Sender::__set_state(array('data' => array('id' => '1105714516202492',),)), 'timestamp' => 1479430034937, 'message' => fritak\MessengerPlatform\Message::__set_state(array('mid' => 23, 'seq' => NULL, 'text' => 'ahoj', 'attachments' => NULL, 'quick_reply' => NULL,)), 'delivery' => NULL, 'optin' => NULL,)),),)),);*/
     }
 
     private function verifyProcess()
