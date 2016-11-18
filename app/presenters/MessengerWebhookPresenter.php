ac450ecd4738e77be6823a246c9a7bf4bcf80e71
David Indra <mail@davidindra.cz>
2016-11-18 01:28:40 +0100

    Fix

--- app/presenters/MessengerWebhookPresenter.php
+++ app/presenters/MessengerWebhookPresenter.php
@@ -2,16 +2,19 @@
 
 namespace App\Presenters;
 
-use Doctrine\Common\Util\Debug;
 use Nette;
 use Tracy\Debugger;
 
 class MessengerWebhookPresenter extends BasePresenter
 {
+    /** @inject @var Nette\Http\Request */
+    public $httpRequest;
+
     private $accessToken = 'EAAP6B4JPne8BABfkuowWxrGHCkK3tbHa25ZC2JY0nJZBibiI9YSZA6Buki8ZByIlHJ8ObZCacA1gmzNI1W7BWGYe9Vy12inseS25VcVxRWabR4nZBUsFAUAXOa4Tjbzw4NKuknPwCFKLvVxt9nkgpr4vnisPwTZAEuTdO1vVq0cGwZDZD';
     private $verifyToken = '2c7dd4efbf3ad50d6d1e2b038df0c0927810fa62';
 
-    private function verifyProcess(){
+    private function verifyProcess()
+    {
         $hub_verify_token = null;
 
         if (isset($_REQUEST['hub_challenge'])) {
@@ -25,8 +28,18 @@ class MessengerWebhookPresenter extends BasePresenter
 
     public function renderDefault()
     {
-        /** @var Nette\Http\Request $httpRequest */
-        $httpRequest = $this->context->getByType('Nette\Http\Request');
-        Debugger::log($httpRequest->getRawBody());
+        //Debugger::log($this->httpRequest->getRawBody());
+        $data = json_decode($this->httpRequest->getRawBody());
+
+        Debugger::log($data);
     }
 }
+
+    /*{
+        "object":"page","entry":[{
+        "id":"560319450832679","time":1479428716398,"messaging":[{
+            "sender":{
+                "id":"1105714516202492"},"recipient":{
+                "id":"560319450832679"},"timestamp":1479428716373,"message":{
+                "mid":"mid.1479428716373:a9fbdb1d34","seq":12,"text":"aha"}}]}]}
+    */
