d39f0bb427beef94f6d3be90496225f08fceb349
David Indra <mail@davidindra.cz>
2016-11-18 01:41:44 +0100

    Fix

--- app/presenters/MessengerWebhookPresenter.php
+++ app/presenters/MessengerWebhookPresenter.php
@@ -50,7 +50,7 @@ class MessengerWebhookPresenter extends BasePresenter
 
         /** @var MessageReceived[] $messages */
         $messages = $bot->getMessagesReceived();
-        foreach($messages as $message){
+        foreach($messages ? $messages : [] as $message){
             $bot->sendMessage($message->messaging['sender']['id'], $message->messaging['message']['text']);
         }
     }
