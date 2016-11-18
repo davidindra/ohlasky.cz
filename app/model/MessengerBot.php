7801e35e5ea4e990305135622cd80b1f180928fe
David Indra <mail@davidindra.cz>
2016-11-18 02:30:02 +0100

    Model

---
+++ app/model/MessengerBot.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Model;
+
+use Nette;
+
+class MessengerBot
+{
+    use Nette\SmartObject;
+
+    private $accessToken = 'EAAP6B4JPne8BABfkuowWxrGHCkK3tbHa25ZC2JY0nJZBibiI9YSZA6Buki8ZByIlHJ8ObZCacA1gmzNI1W7BWGYe9Vy12inseS25VcVxRWabR4nZBUsFAUAXOa4Tjbzw4NKuknPwCFKLvVxt9nkgpr4vnisPwTZAEuTdO1vVq0cGwZDZD';
+    private $verifyToken = '2c7dd4efbf3ad50d6d1e2b038df0c0927810fa62';
+
+    public function __construct()
+    {
+        $this->verifyProcess();
+    }
+
+    private function verifyProcess()
+    {
+        if (isset($_REQUEST['hub_challenge'])) {
+            $challenge = $_REQUEST['hub_challenge'];
+            $hub_verify_token = $_REQUEST['hub_verify_token'];
+
+            if ($hub_verify_token === $this->verifyToken) {
+                print $challenge;
+                exit;
+            }
+        }
+    }
+
+    public function parse($raw){
+        $json = json_decode($raw);
+
+        if(@$json->object != 'page'){
+            exit;
+        }
+
+        foreach(@$json->entry as $entry){
+            foreach(@$entry->messaging as $message){
+                $sender = $message->sender->id;
+                $recipient = $message->recipient->id;
+                $timestamp = $message->timestamp;
+                $seq = $message->message->seq;
+                $text = $message->message->text;
+
+                $this->receivedMessage($sender, $text);
+            }
+        }
+
+        return '';
+    }
+
+    private function receivedMessage($sender, $text){
+        Debugger::log($sender . ': ' . $text);
+    }
+}
+
+class MessengerBotException extends \Exception
+{
+}
--- app/presenters/MessengerWebhookPresenter.php
+++ app/presenters/MessengerWebhookPresenter.php
@@ -2,59 +2,21 @@
 
 namespace App\Presenters;
 
-use fritak\MessengerPlatform;
-use fritak\MessengerPlatform\MessageReceived;
 use Nette;
-use Tracy\Debugger;
 
 class MessengerWebhookPresenter extends BasePresenter
 {
     /** @inject @var Nette\Http\Request */
     public $httpRequest;
 
-    private $accessToken = 'EAAP6B4JPne8BABfkuowWxrGHCkK3tbHa25ZC2JY0nJZBibiI9YSZA6Buki8ZByIlHJ8ObZCacA1gmzNI1W7BWGYe9Vy12inseS25VcVxRWabR4nZBUsFAUAXOa4Tjbzw4NKuknPwCFKLvVxt9nkgpr4vnisPwTZAEuTdO1vVq0cGwZDZD';
-    private $verifyToken = '2c7dd4efbf3ad50d6d1e2b038df0c0927810fa62';
+    /** @inject @var App\Model\MessengerBot */
+    public $bot;
 
     public function renderDefault()
     {
-        $this->verifyProcess();
-
-        //Debugger::log($this->httpRequest->getRawBody());
-        $request = json_decode($this->httpRequest->getRawBody());
-
-        $this->parse($request);
+        $this->template->response = $this->bot->parse($this->httpRequest->getRawBody());
     }
 
-    private function verifyProcess()
-    {
-        if (isset($_REQUEST['hub_challenge'])) {
-            $challenge = $_REQUEST['hub_challenge'];
-            $hub_verify_token = $_REQUEST['hub_verify_token'];
-
-            if ($hub_verify_token === $this->verifyToken) {
-                $this->template->response = $challenge;
-                exit;
-            }
-        }
-    }
-
-    private function parse($json){
-        if(@$json->object != 'page'){
-            $this->terminate();
-        }
-
-        foreach(@$json->entry as $entry){
-            foreach(@$entry->messaging as $message){
-                $sender = $message->sender->id;
-                $recipient = $message->recipient->id;
-                $timestamp = $message->timestamp;
-                $seq = $message->message->seq;
-                $text = $message->message->text;
-
-                Debugger::log($sender . ': ' . $text);
-            }
-        }
-    }
 }
 
 /*
