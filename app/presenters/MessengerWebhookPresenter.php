0cb1d0941838d71c6a682302abfc1a56877c9671
David Indra <mail@davidindra.cz>
2016-11-18 01:20:43 +0100

    Fix

--- app/presenters/MessengerWebhookPresenter.php
+++ app/presenters/MessengerWebhookPresenter.php
@@ -24,6 +24,6 @@ class MessengerWebhookPresenter extends BasePresenter
 
     public function renderDefault()
     {
-        Debugger::log(var_export($this->request->getPost(), true));
+        Debugger::log('post:' . var_export($this->request->getPost(), true));
     }
 }
