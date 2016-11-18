2732303c0287bc08cbab1dc4b80201dbffe83c20
David Indra <mail@davidindra.cz>
2016-11-18 02:00:44 +0100

    Log

--- app/presenters/MessengerWebhookPresenter.php
+++ app/presenters/MessengerWebhookPresenter.php
@@ -37,7 +37,7 @@ class MessengerWebhookPresenter extends BasePresenter
         /** @var MessageReceived[] $messages */
         $messages = $bot->getMessagesReceived();
         foreach ($messages ? $messages : [] as $message) {
-            Debugger::log($message);
+            Debugger::log(json_encode($message));
             //$bot->sendMessage($message->messaging->sender->id, 'text');
         }
 
