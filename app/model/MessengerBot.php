047a79ed0283582db1db0ff36e0b380079666836
David Indra <mail@davidindra.cz>
2016-11-25 18:33:28 +0100

    Fix?

--- app/model/MessengerBot.php
+++ app/model/MessengerBot.php
@@ -187,8 +187,8 @@ class MessengerBot
         Debugger::log(json_encode($wit->entities));
         foreach ($wit->entities as $entityName => $entity) {
             //if(trim(@$entity->value) != ''){
-            $context[$entityName] = $entity->value;
-            Debugger::log($entityName . $entity->value);
+            $context[$entityName] = $entity[0]->value;
+            Debugger::log($entityName . $entity[0]->value);
             //}
         }
         return $context;
